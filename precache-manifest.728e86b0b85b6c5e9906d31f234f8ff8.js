3d8cef00957e450303037722988c45203b3ccad9
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-06-23 22:38:06 -0400

    Updates

---
+++ precache-manifest.728e86b0b85b6c5e9906d31f234f8ff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "1aded1e8b85feb29e823",
+    "url": "/robofriends/static/js/main.1aded1e8.chunk.js"
+  },
+  {
+    "revision": "3444d9518561a666504b",
+    "url": "/robofriends/static/js/1.3444d951.chunk.js"
+  },
+  {
+    "revision": "1aded1e8b85feb29e823",
+    "url": "/robofriends/static/css/main.3f2bf819.chunk.css"
+  },
+  {
+    "revision": "3444d9518561a666504b",
+    "url": "/robofriends/static/css/1.dceab1d1.chunk.css"
+  },
+  {
+    "revision": "4ee709cabacebd71c2b3db6aee1aa00d",
+    "url": "/robofriends/index.html"
+  }
+];
